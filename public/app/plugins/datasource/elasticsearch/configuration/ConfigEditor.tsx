7d770727bf70252d8c6a1747f1e152f279822283
bohandley <brendan.ohandley@gmail.com>
2023-07-07 17:30:42 -0400

    remove test code

--- public/app/plugins/datasource/elasticsearch/configuration/ConfigEditor.tsx
+++ public/app/plugins/datasource/elasticsearch/configuration/ConfigEditor.tsx
@@ -46,7 +46,7 @@ export const ConfigEditor = (props: Props) => {
         dataSourceConfig={options}
         showAccessOptions={showAccessOptions.current}
         onChange={onOptionsChange}
-        sigV4AuthToggleEnabled={true}
+        sigV4AuthToggleEnabled={config.sigV4AuthEnabled}
         renderSigV4Editor={<SIGV4ConnectionConfig {...props}></SIGV4ConnectionConfig>}
         secureSocksDSProxyEnabled={config.secureSocksDSProxyEnabled}
       />
