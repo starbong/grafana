e2aea2cfa03167b30b73509a84ac7136e9d6f089
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-07-11 16:07:06 -0400

    add metrics and tracing to state manager

--- pkg/services/ngalert/metrics/state.go
+++ pkg/services/ngalert/metrics/state.go
@@ -6,7 +6,9 @@ import (
 )
 
 type State struct {
-	AlertState *prometheus.GaugeVec
+	AlertState          *prometheus.GaugeVec
+	StateUpdateDuration *prometheus.HistogramVec
+	StateUpdateCount    *prometheus.CounterVec
 }
 
 func NewStateMetrics(r prometheus.Registerer) *State {
@@ -17,5 +19,24 @@ func NewStateMetrics(r prometheus.Registerer) *State {
 			Name:      "alerts",
 			Help:      "How many alerts by state.",
 		}, []string{"state"}),
+		StateUpdateDuration: promauto.With(r).NewHistogramVec(
+			prometheus.HistogramOpts{
+				Namespace: Namespace,
+				Subsystem: Subsystem,
+				Name:      "state_calculation_duration_milliseconds",
+				Help:      "The duration of calculation of a single state",
+				Buckets:   []float64{10, 25, 50, 100, 500, 1000, 10000, 100000},
+			},
+			[]string{"needsImage"},
+		),
+		StateUpdateCount: promauto.With(r).NewCounterVec(
+			prometheus.CounterOpts{
+				Namespace: Namespace,
+				Subsystem: Subsystem,
+				Name:      "state_calculation_total",
+				Help:      "Total number of state calculations",
+			},
+			[]string{"needsImage"},
+		),
 	}
 }
